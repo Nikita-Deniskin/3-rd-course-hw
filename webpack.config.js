1ee8b1384e39d78671f68eea4fba4502fcf17654
Nikita-Deniskin <ralermr@gmail.com>
2023-08-03 02:07:12 +0500

    3.5

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,15 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const isProduction = process.env.NODE_ENV;
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.ts",
   mode: isProduction ? "production" : "development",
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
       { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -20,6 +25,9 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   optimization: {
     minimizer: ["...", new CssMinimizerPlugin()],
